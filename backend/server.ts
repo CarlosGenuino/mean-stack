42fc2ba59b096c43e3f0eff4158edb54276f72c3
Carlos Genuino <carloxsgenuino3@live.com>
2024-11-11 23:19:17 -0300

    config express

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,16 @@
-import { ENV } from "./utils/env.ts";
-const hello: string = "Hello, World!!!"
-console.log({hello, app_name: ENV.APP_NAME})
+import express, { Request, Response } from 'express'
+import { ENV } from './utils/env.ts'
+const app = express()
+const PORT = Number(ENV.PORT)
+
+app.get("/health", (req: Request, res: Response) => {
+    try {
+        res.status(200).json({success: true, message: "server is running"})
+    } catch(error){
+        res.status(500).json({success: false, message: "server is not running"})
+    }
+});
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on http://localhost:${PORT}`);
+})
